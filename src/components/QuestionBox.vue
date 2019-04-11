ed03b05b99a1278d63037acfe23daaa089f4ec2b
ted555 <thanong984@gmail.com>
2019-04-11 15:58:24 +0700

    first commit and fetch data from API

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="question-box-container">
+        <b-jumbotron>
+            <template slot="lead">
+                Some Question is here?
+            </template>
+
+            <hr class="my-4">
+
+            <p>
+                List of answers
+            </p>
+
+            <b-button variant="primary" href="#">Submiy</b-button>
+            <b-button variant="success" href="#">Next</b-button>
+        </b-jumbotron>
+    </div>
+</template>
